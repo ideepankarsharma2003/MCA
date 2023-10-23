e04495c2a7615c0ac8386e5d8347393f94dc3da1
Lucky <deepankarsharma2003@gmail.com>
2023-10-23 20:17:58 +0530

    finished MCQs

--- Assignments/Programming and Problem Solving/programs_demo/lab_00_mcqs.c
+++ Assignments/Programming and Problem Solving/programs_demo/lab_00_mcqs.c
@@ -9,6 +9,8 @@ int main(){
     // return 0;
 
     // Ques 2.
+    // default parameters are passed to function using ......
+    // a. call by value
     
     // Ques 3. 
     // b. real (not a data type in c)
@@ -17,5 +19,36 @@ int main(){
     // char name[]= "WelcomeHome";
     // printf("%s", name+7); // b. name+7
     // return 0;
+
+    // Ques 5.
+    // Structures can be manipulated using ......... character.
+    // c. None of the above
+
+    // Ques 6.
+    // char str[50];
+    // scanf("%4s", str); // Happy Birthday
+    // printf(str); // c. Happ
+    // return 0;
+
+    // Ques 7.
+    // char str[50]= "Graphic Era University";
+    // printf("%d", sizeof(str)); // a. 50
+    // return 0;
+
+    // Ques 8. 
+    // .......... is not a valid keyword in C.
+    // c. do-while
+
+
+    // Ques 9.
+    // format specifier for double data type is ........
+    // c. %d
+
+    // Ques 10. 
+    char str[50]= "Goodmorning";
+    printf("%d", sizeof(str));
+    printf(/*"Hello World"*/ "How are you?"); // b. 50How are you?
+    return 0;
+
 
 }
