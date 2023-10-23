6385b8da1260966644b07bf4b3e747aff081e65e
Lucky <deepankarsharma2003@gmail.com>
2023-10-23 14:04:47 +0530

    added 4 practicals

---
+++ Assignments/Programming and Problem Solving/programs_demo/lab_00_mcqs.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+
+    // Ques 1.
+    // int j=2;
+    // printf("%d", (++j)++); // b. compilation error
+    // return 0;
+
+    // Ques 2.
+    
+    // Ques 3. 
+    // b. real (not a data type in c)
+
+    // Ques 4. 
+    // char name[]= "WelcomeHome";
+    // printf("%s", name+7); // b. name+7
+    // return 0;
+
+}
